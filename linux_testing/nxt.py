881eecd80a33d43f3ff3ea34e4abcd5a47a045ff
mattschueler <mjschueler@gmail.com>
2018-07-04 22:55:33 -0400

    bt_setup script searches for bricks and pairs them

--- linux_testing/nxt.py
+++ linux_testing/nxt.py
@@ -3,10 +3,6 @@ import serial
 import traceback
 import time
 
-def search():
-	devices = bluetooth.discover_devices(duration=3, lookup_names=True)
-	return devices
-
 s = serial.Serial(port='/dev/rfcomm0', baudrate=9600, bytesize=8, timeout=3.0, write_timeout=3.0)
 
 try:
